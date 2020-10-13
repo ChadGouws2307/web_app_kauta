e206d9120a9268b68df71d10270f3e06d703e9f3
chadgouws <chadgouws013@gmail.com>
2020-10-13 15:12:34 +0200

    Some updates for production env

--- web_app/settings.py
+++ web_app/settings.py
@@ -75,7 +75,7 @@ ROOT_URLCONF = 'web_app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -139,9 +139,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/home/kautuboy/public_html/static'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 LOGIN_REDIRECT_URL = "us-election"
 LOGOUT_REDIRECT_URL = "home"
